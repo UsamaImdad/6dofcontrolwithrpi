eed507546fa9a615e804e3683ff0eac7e93339da
usamaimdad <rreigns273@gmail.com>
2020-03-18 09:24:22 +0500

    Swict statemed removed: if elif Used

--- control_functions.py
+++ control_functions.py
@@ -17,22 +17,22 @@ def abovebasemotor(degree):
 
 
 def midmotor(degree):
-    GPIO.setup(9,GPIO.OUT)
-    p = GPIO.PWM(9,50)
+    GPIO.setup(10,GPIO.OUT)
+    p = GPIO.PWM(10,50)
     # p.start(degree)
     p.ChangeDutyCycle(degree)
     print("Mid motor should move "+str(degree)+" degree")
 
 def clawrotatemotor(degree):
-    GPIO.setup(10,GPIO.OUT)
-    p = GPIO.PWM(10,50)
+    GPIO.setup(11,GPIO.OUT)
+    p = GPIO.PWM(11,50)
     # p.start(degree)
     p.ChangeDutyCycle(degree)
     print("Claw rotate motor should move "+str(degree)+" degree")
 
 def clawmotor(degree):
-    GPIO.setup(11,GPIO.OUT)
-    p = GPIO.PWM(11,50)
+    GPIO.setup(12,GPIO.OUT)
+    p = GPIO.PWM(12,50)
     # p.start(degree)
     p.ChangeDutyCycle(degree)
     print("Claw motor above base should move "+str(degree)+" degree")
@@ -41,11 +41,14 @@ if __name__ == "__main__":
     GPIO.setmode(GPIO.BOARD)
     while True:
         x = input("Enter 1 for base, 2 for above base, 3 for mid motor, 4 for claw rotate motor and 5 for claw motor:")
-        switcher = {
-            1: basemotor(0.5),
-            2: abovebasemotor(0.5),
-            3: midmotor(0.5),
-            4: clawrotatemotor(0.5),
-            5: clawmotor(0.5),
-        }
-        switcher.get(x)
+        v = input("Value:")
+        if x == 1:
+            basemotor(v)
+        elif x == 2:
+            abovebasemotor(v)
+        elif x == 3:
+            midmotor(v)
+        elif x == 4:
+            clawrotatemotor(v)
+        elif x == 5:
+            clawmotor(v)
